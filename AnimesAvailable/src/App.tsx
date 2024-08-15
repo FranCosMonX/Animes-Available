bd8da0d65c077b26820acfbf3cad5d8b4593cea0
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-08-15 14:35:39 -0300

    adicionando as rotas de login e cadastro

--- AnimesAvailable/src/App.tsx
+++ AnimesAvailable/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Cadastro from './components/pages/cadastro/cadastro'
 import Inicio from './components/pages/inicio/inicio'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <Routes>
       <Route index element={<Inicio />} />
+      <Route path='/cadastro' element={<Cadastro />}></Route>
     </Routes>
   )
 }
