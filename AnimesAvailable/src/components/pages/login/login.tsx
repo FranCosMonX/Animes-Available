655b65a4bb8779193196ca19cf0c41b20b54e57b
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-08-15 15:11:30 -0300

    implementando a tela de login

--- AnimesAvailable/src/components/pages/login/login.tsx
+++ AnimesAvailable/src/components/pages/login/login.tsx
@@ -1,7 +1,39 @@
+import { Button, Card, Container, Grid, TextField, Typography } from "@mui/material"
+import { Header } from "../../elementoHTMLEstatico"
+
 const Login = () => {
   return (
-    <>
-    </>
+    <Container disableGutters>
+      <Header />
+      <form>
+        <Card sx={{
+          width: "450px",
+          display: 'flex',
+          flexDirection: 'column',
+          alignItens: 'center',
+          gap: '10px',
+          padding: '10px 5px'
+        }}>
+          <Typography variant="h4" textAlign={"center"}>Login</Typography>
+          <TextField
+            variant="outlined"
+            label="Usuário"
+            type="text"
+            color="secondary"
+          />
+          <TextField
+            variant="outlined"
+            label="Senha"
+            type="password"
+            color="secondary"
+          />
+          <Grid container item display={"flex"} justifyContent={"space-between"}>
+            <Button color="secondary" variant="text" type="button">Cadastrar-se</Button>
+            <Button color="secondary" variant="contained" type="submit">Logar</Button>
+          </Grid>
+        </Card>
+      </form>
+    </Container>
   )
 }
 
