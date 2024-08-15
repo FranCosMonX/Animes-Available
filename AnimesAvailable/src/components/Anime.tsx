4b90d33dd8da7a56407ec2ee016dfd2de804975a
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-08-15 20:50:53 -0300

    adicionando um :hover aos Animes para mostrar as principais informações

--- AnimesAvailable/src/components/Anime.tsx
+++ AnimesAvailable/src/components/Anime.tsx
@@ -1,15 +1,21 @@
+import { Typography } from "@mui/material";
+
 interface Anime {
-  id: number,
-  nome: string,
-  uri: string,
-  alt: string
+  id: number;
+  nome: string;
+  uri: string;
+  alt: string;
+  lancamento?: string;
+  descricao?: string;
+  assistirEm?: string[];
+  nota?: number
 }
 
 interface AnimeParam {
   anime: Anime;
 }
 
-const animeStyle: React.CSSProperties | undefined = {
+const animeStyle: React.CSSProperties = {
   width: '190px',
   height: '270px',
   objectFit: 'cover', //manter a proporção da imagem e ajustando para cobrir toda a área
@@ -19,11 +25,17 @@ const animeStyle: React.CSSProperties | undefined = {
 
 const Anime = ({ anime }: AnimeParam) => {
   return (
-    <img
-      style={animeStyle}
-      src={anime.uri}
-      alt={anime.alt}
-    />
+    <div id="cardAnime">
+      <div id="info">
+        <Typography variant="body2" textAlign={"center"}>{anime.nome}</Typography>
+        <Typography variant="body2" textAlign={"center"}>{anime.nota}</Typography>
+      </div>
+      <img
+        style={animeStyle}
+        src={anime.uri}
+        alt={anime.alt}
+      />
+    </div>
   )
 }
 
--- AnimesAvailable/src/components/pages/animes/todos/todosOsAnimes.tsx
+++ AnimesAvailable/src/components/pages/animes/todos/todosOsAnimes.tsx
@@ -7,48 +7,60 @@ interface AnimeInterface {
   nome: string;
   uri: string;
   alt: string;
+  lancamento?: string;
+  descricao?: string;
+  assistirEm?: string[];
+  nota?: number
 }
 
 const listaAnimes: Array<AnimeInterface> = [{
   id: 1,
   nome: "To Aru",
   uri: "/images/animes/toaruSeries1.jpg",
-  alt: "anime em que o Touma é o protagonista"
+  alt: "anime em que o Touma é o protagonista",
+  nota: 10
 }, {
   id: 2,
   nome: "Toaru no Raiugun",
   uri: "/images/animes/toaruSeries2.jpg",
-  alt: "anime em que a Misaka é a protagonista"
+  alt: "anime em que a Misaka é a protagonista",
+  nota: 10
 }, {
   id: 3,
   nome: "Toaru no Accelerator",
   uri: "/images/animes/toaruSeries3.jpg",
-  alt: "anime em que o Accelerator é o protagonista"
+  alt: "anime em que o Accelerator é o protagonista",
+  nota: 10
 }, {
   id: 4,
   nome: "Arifureta",
   uri: "/images/animes/arifuretaSeries.jpg",
-  alt: "anime em que o Hajime é o protagonista"
+  alt: "anime em que o Hajime é o protagonista",
+  nota: 10
 }, {
   id: 5,
   nome: "Arifureta",
   uri: "/images/animes/arifuretaSeries.jpg",
-  alt: "anime em que o Hajime é o protagonista"
+  alt: "anime em que o Hajime é o protagonista",
+  nota: 10
 }, {
   id: 6,
   nome: "Arifureta",
   uri: "/images/animes/arifuretaSeries.jpg",
-  alt: "anime em que o Hajime é o protagonista"
+  alt: "anime em que o Hajime é o protagonista",
+  nota: 10
 }, {
   id: 7,
   nome: "Toaru no Raiugun",
   uri: "/images/animes/toaruSeries2.jpg",
-  alt: "anime em que a Misaka é a protagonista"
+  alt: "anime em que a Misaka é a protagonista",
+  nota: 10
 }, {
   id: 8,
   nome: "Toaru no Accelerator",
   uri: "/images/animes/toaruSeries3.jpg",
-  alt: "anime em que o Accelerator é o protagonista"
+  alt: "anime em que o Accelerator é o protagonista",
+  nota: 10
 },]
 
 const CatalogoGeral = () => {
