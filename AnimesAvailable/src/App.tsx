48ab561fde5b5ea37ec85c30c0721e87e212f20a
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-08-15 13:11:02 -0300

    melhorando a imagem e o titulo

--- AnimesAvailable/src/App.tsx
+++ AnimesAvailable/src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import './App.css'
 import imagemAnimes from './assets/images/Selecta-Visión-Amazon-Prime-Video.jpg'
 import { Footer, Header } from './components/elementoHTMLEstatico'
@@ -12,11 +12,20 @@ function App() {
         position={'relative'}
         className='main'
       >
-        <h1>Avalie seus filmes favoritos</h1>
         <img
           src={imagemAnimes}
           alt="Imagem contendo alguns atores em filmes de ação encontradas na plataforma da netflix"
         />
+        <Typography
+          position={'absolute'}
+          variant='h1'
+          fontWeight={"bold"}
+          sx={{
+            fontSize: '2.5em',
+            top: '30%',
+            right: '7%'
+          }}
+        >Avalie seus filmes favoritos</Typography>
       </Grid>
       <Footer />
     </>
