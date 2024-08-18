5b52abf2cbec984ca77172f33db7bcb6d672488c
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-08-18 18:20:38 -0300

    adicionando tratamento de erro para a tela de login

--- AnimesAvailable/src/components/pages/login/login.tsx
+++ AnimesAvailable/src/components/pages/login/login.tsx
@@ -1,15 +1,41 @@
+import { zodResolver } from "@hookform/resolvers/zod"
 import { Button, Card, Grid, TextField, Typography } from "@mui/material"
-import { FormEvent } from "react"
+import { SubmitHandler, useForm } from "react-hook-form"
 import { useNavigate } from "react-router-dom"
 import imagemAnimes from '../../../assets/images/Selecta-Visión-Amazon-Prime-Video.jpg'
 import { Base } from "../elementoHTMLEstatico"
 import './login.css'
+import { loginFormData, loginSchema } from "./loginSchema"
 
 const Login = () => {
   const navigate = useNavigate()
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+  // const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault()
+  //   const usuario = {
+  //     logado: true,
+  //     usuario: "teste",
+  //     jogoPreferido: "The Legends of Neverland",
+  //     animePreferido: "Code Geass, Toaru Series, etc",
+  //     hobby: "jogar e assistir animes",
+  //     nomeCompleto: "Conta de Teste",
+  //     email: "teste@gmail.com",
+  //     senha: "teste"
+  //   }
+  //   sessionStorage.setItem("usuario", JSON.stringify(usuario))
+  //   navigate('/animes/todos')
+  // }
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<loginFormData>({
+    resolver: zodResolver(loginSchema),
+  })
+
+  const onSubmit: SubmitHandler<loginFormData> = async (data) => {
+    console.log(data)
     const usuario = {
       logado: true,
       usuario: "teste",
@@ -26,7 +52,7 @@ const Login = () => {
 
   return (
     <Base>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <img
           src={imagemAnimes}
           alt="Imagem contendo alguns atores em filmes de ação encontradas na plataforma da netflix"
@@ -42,16 +68,24 @@ const Login = () => {
         }}>
           <Typography variant="h4" textAlign={"center"}>Login</Typography>
           <TextField
+            id="usuario"
             variant="outlined"
             label="Usuário"
             type="text"
             color="secondary"
+            {...register('usuario')}
+            error={!!errors.usuario}
+            helperText={errors.usuario?.message}
           />
           <TextField
+            id="senha"
             variant="outlined"
             label="Senha"
             type="password"
             color="secondary"
+            {...register('senha')}
+            error={!!errors.senha}
+            helperText={errors.senha?.message}
           />
           <Grid container item display={"flex"} justifyContent={"space-between"}>
             <Button
