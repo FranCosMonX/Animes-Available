1fa5bf3e43993691cf100496dd671be27612a23f
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-08-22 09:50:50 -0300

    habilitando o cors para permitir conexão com qualquer aplicação frontend

--- animes-available/src/main.ts
+++ animes-available/src/main.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './modules/auth/auth.module';
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
   app.useGlobalPipes(new ValidationPipe())
+  app.enableCors()
   await app.listen(3000);
 }
 bootstrap();
