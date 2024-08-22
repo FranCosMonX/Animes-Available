594e7229700af35b909d4d308b2c2d9f525d1a07
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-08-21 22:50:12 -0300

    tornando as rotas privadas por padrao e criando estrategia para assinar uma rota como publica

--- animes-available/src/modules/auth/auth.module.ts
+++ animes-available/src/modules/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthController } from './auth.controller';
+import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 import { jwtConstants } from './constants';
 
@@ -14,7 +16,12 @@ import { jwtConstants } from './constants';
       signOptions: { expiresIn: '3m' },
     }),
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },],
   controllers: [AuthController]
 })
 export class AuthModule { }
